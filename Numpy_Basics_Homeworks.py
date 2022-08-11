cc5a9db0cad59e3aec27b49d3530be6fbbee7b29
Kapturov Alexander <86742866+Ultraluxe25@users.noreply.github.com>
2022-08-11 11:43:11 +0300

    Added Tasks 16-24

--- Numpy_Basics_Homeworks.py
+++ Numpy_Basics_Homeworks.py
@@ -154,11 +154,111 @@ def solution(arr1, arr2):
     return arr
 
 
-
 # Task 15. На вход подается некоторый одномерный массив. Занулите первые 3 элемента.
 
 import numpy as np
 
 def solution(arr):
     arr[:3] = 0
+    return arr
+
+
+# Task 16. На вход подается двумерный массив (матрица):
+# https://ucarecdn.com/d2707fa4-3105-46da-a1d7-0a4ca4e40362/
+# вытяните из матрицы указанную область.
+
+""" arr для задач 16-24
+arr = [[10, 12, 14, 18, 20],
+       [ 8, 22, 18, 19, 12],
+       [ 4, 14, 84, 38, 11],
+       [ 2, 32,  4, 31,  9],
+       [ 1, 11,  1,  8,  6]]
+"""
+
+import numpy as np
+
+def solution(arr):
+    arr = arr[:2, :2]
+    return arr
+
+
+# Task 17. На вход подается двумерный массив (матрица):
+# https://ucarecdn.com/7f96bccf-dce4-430f-8f5e-9842a24b5000/
+# вытяните из матрицы указанную область.
+
+import numpy as np
+
+def solution(arr):
+    arr = arr[1:3, 3:]
+    return arr
+
+
+# Task 18. Выполните поэлементное сложение двух столбцов:
+# https://ucarecdn.com/1d6c0c70-0f78-4827-8f21-072923404f5d/
+
+import numpy as np
+
+def solution(arr):
+    arr = arr[:, 0] + arr[:, 3]
+    return arr
+
+
+# Task 19. Выполните поэлементное сложение двух столбцов:
+# https://ucarecdn.com/7531165c-7236-4a03-af90-970f76bdf2e1/
+
+import numpy as np
+
+def solution(arr):
+    arr = arr[1] + arr[3]
+    return arr
+
+
+# Task 20. Выполните поэлементное сложение двух столбцов:
+# https://ucarecdn.com/7a2be0a9-154f-4324-a1c3-86413db31247/
+
+import numpy as np
+
+def solution(arr):
+    arr = arr[1:4, 1] + arr[1:4, 3]
+    return arr
+
+
+# Task 21. Выполните поэлементное сложение двух столбцов:
+# https://ucarecdn.com/2f1ff817-4521-44d2-8b96-bf866ef9e932/
+
+import numpy as np
+
+def solution(arr):
+    arr = arr[0, :4] + arr[4, 1:]
+    return arr
+
+
+# Task 22. Выполните поэлементное сложение двух столбцов:
+# https://ucarecdn.com/faa3d37f-a16c-49b0-aeb7-e45512d9dcf7/
+
+import numpy as np
+
+def solution(arr):
+    arr = arr[1:4, 1] + arr[2:, 2]
+    return arr
+
+
+# Task 23. Выполните поэлементное сложение двух столбцов:
+# https://ucarecdn.com/2bf71390-a37e-4420-a0e9-27f3b3ce6b06/
+
+import numpy as np
+
+def solution(arr):
+    arr = arr[2:, :2] + arr[:3, 3:]
+    return arr
+
+
+# Task 24. ыполните поэлементное сравнение двух столбцов:
+# https://ucarecdn.com/21f66abd-1a69-444c-9138-eeaf8d9d54de/
+# если значение в первом столбце больше чем значение во втором, тогда в результирующем массиве должно быть True, иначе False
+
+import numpy as np
+
+def solution(arr):
+    arr = arr[:, 0] > arr[:, 2]
     return arr
